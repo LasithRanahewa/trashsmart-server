5eae2617b6e626228363033baae03f520a719337
RusaraBW <rusara.wimalasena123@gmail.com>
2024-08-03 19:41:43 +0530

    initial folder structure

---
+++ src/main/java/com/example/trashsmart/TrashsmartApplication.java
@@ -0,0 +1,13 @@
+package com.example.trashsmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrashsmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrashsmartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/trashsmart/TrashsmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trashsmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrashsmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
