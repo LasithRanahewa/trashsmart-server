145bddf84d0e7db8e3b8ce99c0b67be589e623dc
RusaraBW <rusara.wimalasena123@gmail.com>
2024-08-17 19:40:38 +0530

    superclasses

---
+++ src/main/java/com/g41/trashsmart_server/Models/SystemUser.java
@@ -0,0 +1,57 @@
+package com.g41.trashsmart_server.Models;
+
+import com.g41.trashsmart_server.Enums.Role;
+import jakarta.persistence.MappedSuperclass;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+public abstract class SystemUser extends User {
+    private LocalDate dob;
+    private String nic;
+
+    public SystemUser() {
+    }
+
+    // Contactor registration
+    public SystemUser(String firstName, String lastName, String email, String password, Role role,
+                      LocalDateTime createdTimeStamp, LocalDate dob, String nic) {
+        super(firstName, lastName, email, password, role, createdTimeStamp);
+        this.dob = dob;
+        this.nic = nic;
+    }
+
+    // Driver/Cleaner ContactNo Registration no Email
+    public SystemUser(String firstName, String lastName, String password, String contactNo, String address, Role role,
+                      String profileURL, LocalDateTime createdTimeStamp, LocalDate dob, String nic) {
+        super(firstName, lastName, password, contactNo, address, role, profileURL, createdTimeStamp);
+        this.dob = dob;
+        this.nic = nic;
+    }
+
+    // Driver/Cleaner Email Registration
+    public SystemUser(String firstName, String lastName, String password, String contactNo, String address, Role role,
+                      String email, String profileURL, LocalDateTime createdTimeStamp, LocalDate dob, String nic) {
+        super(firstName, lastName, password, contactNo, address, role, email, profileURL, createdTimeStamp);
+        this.dob = dob;
+        this.nic = nic;
+    }
+
+    // Getters and Setters
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public String getNic() {
+        return nic;
+    }
+
+    public void setNic(String nic) {
+        this.nic = nic;
+    }
+}
