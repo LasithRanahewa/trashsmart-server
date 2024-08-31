753fc39818085210855df04990e0a6b430d298fa
RusaraBW <rusara.wimalasena123@gmail.com>
2024-08-31 17:10:04 +0530

    WCR service

---
+++ src/main/java/com/g41/trashsmart_server/Services/WasteCollectionRequestService.java
@@ -0,0 +1,87 @@
+package com.g41.trashsmart_server.Services;
+
+import com.g41.trashsmart_server.Models.Organization;
+import com.g41.trashsmart_server.Models.WasteCollectionRequest;
+import com.g41.trashsmart_server.Repositories.OrganizationRepository;
+import com.g41.trashsmart_server.Repositories.WasteCollectionRequestRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class WasteCollectionRequestService {
+    private final WasteCollectionRequestRepository wasteCollectionRequestRepository;
+    private final OrganizationRepository organizationRepository;
+
+    public WasteCollectionRequestService(WasteCollectionRequestRepository wasteCollectionRequestRepository,
+                                         OrganizationRepository organizationRepository) {
+        this.wasteCollectionRequestRepository = wasteCollectionRequestRepository;
+        this.organizationRepository = organizationRepository;
+    }
+
+    // Retrieve all waste collection requests
+    public List<WasteCollectionRequest> getAllRequests() {
+        return wasteCollectionRequestRepository.findAll();
+    }
+
+    // Retrieve specific by id
+    public WasteCollectionRequest getRequest(Long id) {
+        Optional<WasteCollectionRequest> wasteCollectionRequestOptional = wasteCollectionRequestRepository.findById(id);
+        if (wasteCollectionRequestOptional.isEmpty()) {
+           throw new IllegalStateException("Waste Collection Request with id " + id + " does not exist");
+        }
+        return wasteCollectionRequestOptional.get();
+    }
+
+    // Create a new waste collection request
+    public void createRequest(WasteCollectionRequest wasteCollectionRequest, Long organizationId) {
+        Optional<Organization> organizationOptional = organizationRepository.findById(organizationId);
+        if (organizationOptional.isEmpty()) {
+            throw new IllegalStateException("Organization with id " + organizationId + " does not exist");
+        }
+        Organization organization = organizationOptional.get();
+        wasteCollectionRequest.setOrganization(organization);
+        wasteCollectionRequestRepository.save(wasteCollectionRequest);
+    }
+
+    // Delete a waste collection request
+    public void deleteRequest(Long id) {
+        Optional<WasteCollectionRequest> wasteCollectionRequestOptional = wasteCollectionRequestRepository.findById(id);
+        if (wasteCollectionRequestOptional.isEmpty()) {
+            throw new IllegalStateException("Waste Collection Request with id " + id + " does not exist");
+        }
+        wasteCollectionRequestRepository.deleteById(id);
+    }
+
+    // Update a waste collection request
+    public void updateRequest(Long id, WasteCollectionRequest wasteCollectionRequest) {
+        WasteCollectionRequest wasteCollectionRequestToUpdate = wasteCollectionRequestRepository.findById(id).orElseThrow(
+                () -> new IllegalStateException("Waste Collection Request with id " + id + " does not exist")
+        );
+        if (wasteCollectionRequest.getAccumulatedVolume() != null &&
+                !wasteCollectionRequestToUpdate.getAccumulatedVolume().equals(wasteCollectionRequest.getAccumulatedVolume())) {
+            wasteCollectionRequestToUpdate.setAccumulatedVolume(wasteCollectionRequest.getAccumulatedVolume());
+        }
+        if (wasteCollectionRequest.getWasteType() != null &&
+                !wasteCollectionRequestToUpdate.getWasteType().equals(wasteCollectionRequest.getWasteType())) {
+            wasteCollectionRequestToUpdate.setWasteType(wasteCollectionRequest.getWasteType());
+        }
+        if (wasteCollectionRequest.getLongitude() != null &&
+                !wasteCollectionRequestToUpdate.getLongitude().equals(wasteCollectionRequest.getLongitude())) {
+            wasteCollectionRequestToUpdate.setLongitude(wasteCollectionRequest.getLongitude());
+        }
+        if (wasteCollectionRequest.getLatitude() != null &&
+                !wasteCollectionRequestToUpdate.getLatitude().equals(wasteCollectionRequest.getLatitude())) {
+            wasteCollectionRequestToUpdate.setLatitude(wasteCollectionRequest.getLatitude());
+        }
+        if (wasteCollectionRequest.getWasteCollectionRequestStatus() != null &&
+                !wasteCollectionRequestToUpdate.getWasteCollectionRequestStatus()
+                        .equals(wasteCollectionRequest.getWasteCollectionRequestStatus())) {
+            wasteCollectionRequestToUpdate.setWasteCollectionRequestStatus(
+                    wasteCollectionRequest.getWasteCollectionRequestStatus()
+            );
+        }
+        wasteCollectionRequestRepository.save(wasteCollectionRequestToUpdate);
+    }
+}
