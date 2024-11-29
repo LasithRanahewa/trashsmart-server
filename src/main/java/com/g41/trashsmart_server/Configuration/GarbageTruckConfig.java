e52e2dfc65ccfd66679980008dcca3f0b4945ef9
RusaraBW <rusara.wimalasena123@gmail.com>
2024-11-29 15:36:07 +0530

    add more drivers and trucks to config

--- src/main/java/com/g41/trashsmart_server/Configuration/GarbageTruckConfig.java
+++ src/main/java/com/g41/trashsmart_server/Configuration/GarbageTruckConfig.java
@@ -17,14 +17,47 @@ public class GarbageTruckConfig {
             GarbageTruck ABC1234 = new GarbageTruck(
                     "ABC1234",
                     23460,
-                    100
+                    1700
             );
             GarbageTruck DEF1234 = new GarbageTruck(
                     "DEF1234",
                     35678,
-                    450
+                    4500
             );
-            garbageTruckRepository.saveAll(List.of(ABC1234, DEF1234));
+            GarbageTruck AAB1234 = new GarbageTruck(
+                    "AAB1234",
+                    36822,
+                    5100
+            );
+            GarbageTruck AAC1234 = new GarbageTruck(
+                    "AAC1234",
+                    56781,
+                    2300
+            );
+            GarbageTruck AAD1234 = new GarbageTruck(
+                    "AAD1234",
+                    35600,
+                    4800
+            );
+            GarbageTruck AAE1234 = new GarbageTruck(
+                    "AAE1234",
+                    67823,
+                    3100
+            );
+            GarbageTruck AAA1234 = new GarbageTruck(
+                    "AAA1234",
+                    36780,
+                    2500
+            );
+            garbageTruckRepository.saveAll(List.of(
+                    ABC1234,
+                    DEF1234,
+                    AAB1234,
+                    AAC1234,
+                    AAD1234,
+                    AAE1234,
+                    AAA1234
+            ));
         };
     }
 }
