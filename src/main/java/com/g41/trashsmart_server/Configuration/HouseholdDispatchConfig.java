e1e8f32fe72f146dc34f692da33fab00eed5f9c9
RusaraBW <rusara.wimalasena123@gmail.com>
2024-12-01 19:03:50 +0530

    temp

--- src/main/java/com/g41/trashsmart_server/Configuration/HouseholdDispatchConfig.java
+++ src/main/java/com/g41/trashsmart_server/Configuration/HouseholdDispatchConfig.java
@@ -7,14 +7,19 @@ import com.g41.trashsmart_server.Models.HouseholdDispatch;
 import com.g41.trashsmart_server.Repositories.DriverRepository;
 import com.g41.trashsmart_server.Repositories.GarbageTruckRepository;
 import com.g41.trashsmart_server.Repositories.HouseholdDispatchRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Configuration
 public class HouseholdDispatchConfig {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Bean
     CommandLineRunner householdDispatchCommandLineRunner(
@@ -23,7 +28,17 @@ public class HouseholdDispatchConfig {
             GarbageTruckRepository garbageTruckRepository) {
         return args -> {
             GarbageTruck garbageTruck = new GarbageTruck();
-            Driver driver = new Driver();
+            Driver driver = new Driver(
+                    "Kavisha",
+                    "Wanigarathne",
+                    passwordEncoder.encode("password123"),
+                    "0712990644",
+                    "55, Reid Avenue, Colombo 07",
+                    "kavisha@gmail.com",
+                    "www.google.com",
+                    LocalDate.of(1995, 12, 12),
+                    "200168686836"
+            );
 
             // Save the transient instances first
             garbageTruckRepository.save(garbageTruck);
