16ae66d24736cf170d77c7d913e9035147aeaa30
RusaraBW <rusara.wimalasena123@gmail.com>
2024-12-03 18:49:21 +0530

    fix communal bin constructor

--- src/main/java/com/g41/trashsmart_server/Configuration/CommunalBinConfig.java
+++ src/main/java/com/g41/trashsmart_server/Configuration/CommunalBinConfig.java
@@ -22,16 +22,14 @@ public class CommunalBinConfig {
                     6.927079,
                     79.861244,
                     WasteType.NON_BIO_DEGRADABLE,
-                    BinSize.MEGA,
-                    LocalDate.of(2024,8,10)
+                    BinSize.MEGA
             );
             Bin001.setFillLevel(80.0);
             CommunalBin Bin002 = new CommunalBin(
                     6.927079,
                     79.861244,
                     WasteType.NON_BIO_DEGRADABLE,
-                    BinSize.MEGA,
-                    LocalDate.of(2024,10,15)
+                    BinSize.MEGA
             );
             Bin002.setFillLevel(45.0);
             communalBinRepository.saveAll(List.of(Bin001, Bin002));
