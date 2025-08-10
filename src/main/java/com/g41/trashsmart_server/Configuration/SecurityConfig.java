b21a0828d829c4d300ea1a40c501d21ac15c49cb
Lasith Ranahewa <lasithmrana@gmail.com>
2025-08-10 21:02:19 +0530

    update CORS configuration to use allowedOriginPatterns

--- src/main/java/com/g41/trashsmart_server/Configuration/SecurityConfig.java
+++ src/main/java/com/g41/trashsmart_server/Configuration/SecurityConfig.java
@@ -71,7 +71,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of(origins.split(",")));
+        configuration.setAllowedOriginPatterns(List.of(origins.split(",")));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
         configuration.setAllowCredentials(true);
