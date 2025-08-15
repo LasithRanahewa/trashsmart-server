7b8987af9f55b5f5a980252422e5023c5c836f9b
RusaraBW <rusara.wimalasena123@gmail.com>
2025-08-15 23:11:31 +0530

    cluster service integration

---
+++ src/main/java/com/g41/trashsmart_server/Configuration/FastApiConfig.java
@@ -0,0 +1,16 @@
+package com.g41.trashsmart_server.Configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class FastApiConfig {
+    @Bean
+    public WebClient fastApiWebClient(@Value("${FAST_API_URL:http://localhost:8000/api}") String fastApiUrl) {
+        return WebClient.builder()
+                .baseUrl(fastApiUrl)
+                .build();
+    }
+}
